ead878076ffded5249ac63a71e7a7197ac1b4dce
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-05-20 21:33:34 +0530

    Added validation error response in GlobalExceptionHandler

--- src/main/java/com/OPD/exception/GlobalExceptionHandler.java
+++ src/main/java/com/OPD/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,5 +20,16 @@ public class GlobalExceptionHandler {
 			error.put("message", e.getMessage());
 			
 			return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException ex){
+		Map<String,String> errors=new HashMap<>();
+		
+		ex.getBindingResult().getFieldErrors().forEach(error->{
+			errors.put(error.getField(), error.getDefaultMessage());
+		});
+		
+		return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
 	}
 }
