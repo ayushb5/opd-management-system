11e4a5c0e7657e80475470ae22438c0b6b9517c3
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-05-23 20:42:49 +0530

    added some packages

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Dashboard from "../pages/dashboard/Dashboard"
+import PatientList from "../pages/patient/PatientList"
+import AddPatient from "../pages/patient/AddPatient"
+import MainLayout from "../layouts/MainLayout"
+import DoctorList from "../pages/doctor/DoctorList"
+import AddDoctor from "../pages/doctor/AddDoctor"
+import EditDoctor from "../pages/doctor/EditDoctor"
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Dashboard />}></Route>
-            </Routes>
-        </BrowserRouter>
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/patients" element={<PatientList />} />
+                    <Route path="/patients/add-patient" element={<AddPatient />} />
+                    <Route path="/doctors" element={<DoctorList />} />
+                    <Route path="/doctors/add-doctor" element={<AddDoctor />} />
+                    <Route path="/doctors/edit-doctor/:id" element={<EditDoctor />} />
+                </Route>
+            </Routes >
+        </BrowserRouter >
     )
 }
 
