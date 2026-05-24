1389b16131a95e1631c9459be8336ed7148177b6
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-05-24 20:23:09 +0530

    feat: added Patient list table format with actions to Edit and Delete

--- frontend/src/pages/patient/PatientList.jsx
+++ frontend/src/pages/patient/PatientList.jsx
@@ -1,19 +1,32 @@
 import { useEffect, useState } from "react"
-import { getAllPatients } from "../../services/patientService"
+import { deletePatient, getAllPatients } from "../../services/patientService"
 import { NavLink } from "react-router-dom";
+import Spinner from "../../components/common/Spinner";
+import { toast } from "react-toastify";
+import ConfirmModal from "../../components/common/ConfirmModal";
 
 function PatientList() {
     const [patients, setPatients] = useState([]);
     const [filteredPatients, setFilteredPatients] = useState([]);
     const [search, setSearch] = useState("");
 
+    const [showModal, setShowModal] = useState(false);
+    const [selectedPatientId, setSelectedPatientId] = useState(null);
+
+    const [loading, setLoading] = useState(false);
+    const [deleteLoading, setDeleteLoading] = useState(false);
+
     const fetchPatients = async () => {
         try {
+            setLoading(true);
             const data = await getAllPatients();
             setPatients(data);
             setFilteredPatients(data);
         } catch (error) {
+            toast.error("Failed to load patients")
             console.log(error);
+        } finally {
+            setLoading(false);
         }
     }
 
@@ -25,15 +38,27 @@ function PatientList() {
             return;
         }
         const filtered = patients.filter((patient) =>
-            patient.name
+            patient.patient_name
                 .toLowerCase()
                 .includes(value.toLowerCase())
         );
         setFilteredPatients(filtered);
     }
 
-    const handleDelete = (id) => {
-        console.log("Delete patient: ", id);
+    const confirmDelete = async () => {
+        try {
+            setDeleteLoading(true);
+            await deletePatient(selectedPatientId);
+            setShowModal(false);
+            setSelectedPatientId(null);
+            toast.success("Patient deleted successfully");
+            fetchPatients();
+        } catch (error) {
+            toast.error("Failed to delete patient");
+            console.log(error);
+        } finally {
+            setDeleteLoading(false);
+        }
     }
 
     useEffect(() => {
@@ -51,48 +76,53 @@ function PatientList() {
                     <tr>
                         <th>S.No</th>
                         <th>Name</th>
+                        <th>Doctor</th>
                         <th>Age</th>
                         <th>Gender</th>
                         <th>Mobile</th>
                         <th>Blood Group</th>
-                        <th>Height</th>
-                        <th>Smoking</th>
-                        <th>Alcohol</th>
-                        <th>Tobacco</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {filteredPatients.length > 0 ? (
-                        filteredPatients.map((patient) => (
+                    {loading ? (
+                        <tr>
+                            <td colSpan={"8"} className="text-center">
+                                <Spinner />
+                            </td>
+                        </tr>
+                    ) : filteredPatients.length > 0 ? (
+                        filteredPatients.map((patient, index) => (
                             <tr key={patient.id}>
-                                <td>{patient.id}</td>
-                                <td>{patient.name}</td>
+                                <td>{index + 1}</td>
+                                <td>{patient.patient_name}</td>
+                                <td>{patient.doctor.name}</td>
                                 <td>{patient.age}</td>
                                 <td>{patient.gender}</td>
                                 <td>{patient.mobileno}</td>
                                 <td>{patient.blood_group}</td>
-                                <td>{patient.height}</td>
-                                <td>{patient.smoking}</td>
-                                <td>{patient.alcohol}</td>
-                                <td>{patient.tobacco}</td>
                                 <td>
                                     <div className="d-flex gap-2">
-                                        <NavLink to={`/edit-patient/${patient.id}`} className={"btn btn-warning btn-sm"}>Edit</NavLink>
-                                        <button onClick={() => handleDelete(patient.id)} className={"btn btn-danger btn-sm"}>Delete</button>
+                                        <NavLink to={`/patients/edit-patient/${patient.id}`} className={"btn btn-warning btn-sm"}>Edit</NavLink>
+                                        <button onClick={() => {
+                                            setSelectedPatientId(patient.id);
+                                            setShowModal(true);
+                                        }} className={"btn btn-danger btn-sm"}>Delete</button>
                                     </div>
                                 </td>
                             </tr>
                         ))
                     ) : (
                         <tr>
-                            <td colSpan={"11"} className="text-center">
+                            <td colSpan={"8"} className="text-center">
                                 No Patients found
                             </td>
                         </tr>
                     )}
                 </tbody>
             </table>
+
+            <ConfirmModal show={showModal} title="Delete Patient" message="Are you sure you want to delete this patient?" onConfirm={confirmDelete} onClose={() => setShowModal(false)} deleteLoading={deleteLoading} />
         </ >
     )
 }
