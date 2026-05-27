f6bf3c503d8c8b2a3eb93a10393ea361931b9d6b
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-05-27 21:39:59 +0530

    feat: Jwt Service Implementation

---
+++ backend/src/main/java/com/OPD/serviceImpl/JwtServiceImpl.java
@@ -0,0 +1,65 @@
+package com.OPD.serviceImpl;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.OPD.services.JwtService;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+@Service
+public class JwtServiceImpl implements JwtService {
+
+	@Value("${jwt.secret}")
+	private String secretKey;
+	
+	@Value("${jwt.expiration}")
+	private long jwtExpiration;
+	
+	@Override
+	public String generateToken(String email) {
+		return Jwts.builder()
+				.setSubject(email)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis()+jwtExpiration))
+				.signWith(getSignInKey(),SignatureAlgorithm.HS256)
+				.compact();
+	}
+
+	@Override
+	public String extractUsername(String token) {
+		return extractAllClaims(token).getSubject();
+	}
+
+	@Override
+	public boolean isTokenValid(String token, String email) {
+		final String username=extractUsername(token);
+		return username.equals(email) && !isTokenExpired(token);
+	}
+	
+	private boolean isTokenExpired(String token) {
+		return extractAllClaims(token)
+				.getExpiration()
+				.before(new Date());
+	}
+	
+	private Claims extractAllClaims(String token) {
+		return Jwts.parserBuilder()
+				.setSigningKey(getSignInKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+	}
+	
+	private Key getSignInKey() {
+		byte[] keyBytes=Decoders.BASE64.decode(secretKey);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+}
