5679da26af6fbc73fb11c82ce9ec994b943f015a
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-04 21:07:33 +0530

    feat: added package react-toastify with ToastContainer in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <ToastContainer position='top-right' autoClose={3000} />
+    </BrowserRouter>
   </StrictMode>,
 )
