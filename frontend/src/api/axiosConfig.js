04b6956b4a2de597dc979943375533cb977e9452
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-04 21:04:35 +0530

    refractor: renaming function

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
+const axiosInstance = axios.create({
   baseURL: "http://localhost:8080",
 });
 
-export default API;
+export default axiosInstance;
