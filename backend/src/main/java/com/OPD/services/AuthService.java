c53ee6dbe46848f0bb9892ed1faee5c62f99cd9a
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-04 21:16:16 +0530

    refractor: taking just login service instead of 3 services

--- backend/src/main/java/com/OPD/services/AuthService.java
+++ backend/src/main/java/com/OPD/services/AuthService.java
@@ -1,9 +1,11 @@
 package com.OPD.services;
 
 import com.OPD.dto.LoginDto;
+import com.OPD.response.LoginResponse;
 
 public interface AuthService {
-	String adminLogin(LoginDto loginDto);
-	String doctorLogin(LoginDto loginDto);
-	String receptionistLogin(LoginDto loginDto);
+//	LoginResponse adminLogin(LoginDto loginDto);
+//	LoginResponse doctorLogin(LoginDto loginDto);
+//	LoginResponse receptionistLogin(LoginDto loginDto);
+	LoginResponse login(LoginDto loginDto);
 }
