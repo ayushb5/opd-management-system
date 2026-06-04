92a3547b27e72aacf90092607a183297f88bd101
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-04 21:12:09 +0530

    feat: added routes functionality for proper navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import Login from "./pages/Login"
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import DoctorDashboard from "./pages/doctor/DoctorDashboard";
+import ReceptionistDashboard from "./pages/receptionist/ReceptionistDashboard";
+import DashboardLayout from "./layouts/DashboardLayout";
 
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route element={<DashboardLayout />}>
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
+        <Route path="/receptionist/dashboard" element={<ReceptionistDashboard />} />
+      </Route>
+    </Routes>
   )
 }
 
