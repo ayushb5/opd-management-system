a1d47647fd040ebf9c0f267998e3e1ba246313a3
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-06 21:31:55 +0530

    feat: Added Dashboard Card reusable component

---
+++ frontend/src/components/DashboardCard.jsx
@@ -0,0 +1,20 @@
+function DashboardCard({ title, count, icon }) {
+    return (
+        <div className="card shadow-sm border-0 h-100">
+            <div className="card-body">
+                <div className="d-flex justify-content-between align-items-center">
+                    <div>
+                        <h6 className="text-muted">{title}</h6>
+                        <h3>{count}</h3>
+                    </div>
+
+                    <div className="fs-1 text-primary">
+                        {icon}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DashboardCard
