c1d1c6f83bcc333b19aac97dd877615fc645555e
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-06 21:31:12 +0530

    refractor: added comment

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,5 +1,6 @@
 import axiosInstance from "../api/axiosConfig";
 
+// Login
 export const login = async (loginData) => {
   const response = await axiosInstance.post("/auth/login", loginData);
   return response.data;
