645a259e191e243c803602bb7679d708e096b29d
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-06 21:25:42 +0530

    refractor: new routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,67 @@
 import Login from "./pages/Login"
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import DashboardLayout from "./layouts/DashboardLayout";
+import ProtectedRoute from "./routes/ProtectedRoute";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import DoctorDashboard from "./pages/doctor/DoctorDashboard";
 import ReceptionistDashboard from "./pages/receptionist/ReceptionistDashboard";
-import DashboardLayout from "./layouts/DashboardLayout";
+import DoctorList from "./pages/admin/doctors/DoctorList";
+import AddDoctor from "./pages/admin/doctors/AddDoctor";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+
       <Route element={<DashboardLayout />}>
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
-        <Route path="/receptionist/dashboard" element={<ReceptionistDashboard />} />
+
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute allowedRole="ADMIN">
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/doctors/"
+        >
+          <Route index element={
+            <ProtectedRoute allowedRole="ADMIN">
+              <DoctorList />
+            </ProtectedRoute>
+          }
+          />
+
+          <Route
+            path="add-doctor"
+            element={
+              <ProtectedRoute allowedRole="ADMIN">
+                <AddDoctor />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+
+        <Route
+          path="/doctor/dashboard"
+          element={
+            <ProtectedRoute allowedRole="DOCTOR">
+              <DoctorDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/receptionist/dashboard"
+          element={
+            <ProtectedRoute allowedRole="RECEPTIONIST">
+              <ReceptionistDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Route>
     </Routes>
   )
