883131e9b45c947e87ea9118420b3f46ae5125a9
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-06 21:29:49 +0530

    refractor: Added responsive sidebar with bootstrap offcanvas

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,23 +1,35 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import Sidebar from "../components/Sidebar"
-import { useState } from "react"
 
 function DashboardLayout() {
-    const [showSidebar, setShowSidebar] = useState(false);
 
     return (
         <div className="container-fluid">
             <div className="row min-vh-100">
                 <div className="col-lg-2 p-0 d-none d-lg-block">
-                    <Sidebar />
+                    <Sidebar showTitle={true} />
                 </div>
-                <div className="offcanvas offcanvas-start" id="sidebarOffCanvas" tabIndex={-1}>
-                    <div className="offcanvas-body p-0">
-                        <Sidebar />
+                <div
+                    className="offcanvas offcanvas-start p-0"
+                    id="sidebarOffCanvas"
+                    tabIndex={-1}
+                    style={{ width: "280px" }}
+                >
+                    <div className="offcanvas-header bg-primary">
+                        <h5 className="text-white">City Care Hospital</h5>
+                        <button
+                            type="button"
+                            className="btn-close mb-1"
+                            data-bs-dismiss="offcanvas"
+                        ></button>
+                    </div>
+
+                    <div className="offcanvas-body p-0 overflow-hidden">
+                        <Sidebar showTitle={false} />
                     </div>
                 </div>
-                <div className="col-lg-10 p-0">
+                <div className="col-12 col-lg-10 p-0">
                     <Navbar />
                     <div className="p-4">
                         <Outlet />
