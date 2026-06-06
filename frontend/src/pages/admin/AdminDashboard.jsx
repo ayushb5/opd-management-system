08ffcaf3fb0c72d879b4ed5cdb816be012922200
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-06 21:30:44 +0530

    feat: Added Dashboard cards

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,6 +1,23 @@
+import DashboardCard from "../../components/DashboardCard"
+import { PersonBadge, People, Person, CalendarCheck } from "react-bootstrap-icons"
 function AdminDashboard() {
     return (
-        <div>Admin Dashboard</div>
+        <div className="container-fluid">
+            <div className="row g-4">
+                <div className="col-12 col-sm-6 col-xl-3">
+                    <DashboardCard title={"Total Doctors"} count={12} icon={<PersonBadge />} />
+                </div>
+                <div className="col-12 col-sm-6 col-xl-3">
+                    <DashboardCard title={"Total Receptionist"} count={5} icon={<People />} />
+                </div>
+                <div className="col-12 col-sm-6 col-xl-3">
+                    <DashboardCard title={"Total Patients"} count={120} icon={<Person />} />
+                </div>
+                <div className="col-12 col-sm-6 col-xl-3">
+                    <DashboardCard title={"Total Appointments"} count={18} icon={<CalendarCheck />} />
+                </div>
+            </div>
+        </div>
     )
 }
 
