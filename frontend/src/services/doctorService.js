85eca267614b7a91430616a275359dfa829d3d24
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-07 20:04:47 +0530

    refractor: added service for get doctor by Id and delete doctor by Id

--- frontend/src/services/doctorService.js
+++ frontend/src/services/doctorService.js
@@ -11,3 +11,21 @@ export const getDoctors = async () => {
   const response = await axiosInstance.get("/doctor");
   return response.data;
 };
+
+//Get doctor by Id
+export const getDoctorById = async (id) => {
+  const response = await axiosInstance.get(`/doctor/${id}`);
+  return response.data;
+};
+
+// Update doctor by Id
+export const updateDoctor = async (id, doctorData) => {
+  const response = await axiosInstance.put(`/doctor/${id}`, doctorData);
+  return response.data;
+};
+
+//Delete doctor by Id
+export const deleteDoctor = async (id) => {
+  const response = await axiosInstance.delete(`/doctor/${id}`);
+  return response.data;
+};
