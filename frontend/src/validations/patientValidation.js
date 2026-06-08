e5d667fd70329b0f0413b8365d81d61d8a4ffc44
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-08 12:19:43 +0530

    feat: Designed Validation schema for patient

---
+++ frontend/src/validations/patientValidation.js
@@ -0,0 +1,26 @@
+import * as Yup from "yup";
+
+export const patientValidationSchema = () =>
+  Yup.object({
+    patient_name: Yup.string().required("Patient name is required"),
+    age: Yup.number()
+      .typeError("Age must be a number")
+      .min(0, "Age cannot be negative")
+      .required("Age is required"),
+    gender: Yup.string().required("Gender is required"),
+    mobileno: Yup.string()
+      .matches(/^[0-9]{10}$/, "Enter valid mobile number")
+      .required("Mobile is required"),
+    address: Yup.string().required("Address is required"),
+    blood_group: Yup.string().required("Blood group is required"),
+    height: Yup.number()
+      .typeError("Height must be a number")
+      .required("Height is required")
+      .positive("Height must be positive"),
+    smoking: Yup.string().required("Smoking is required"),
+    alcohol: Yup.string().required("Alcohol is required"),
+    tobacco: Yup.string().required("Tobacco is required"),
+    doctorId: Yup.number()
+      .typeError("Doctor is required")
+      .required("Doctor is required"),
+  });
