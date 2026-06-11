51f0a16a2730bafa74c6cd1d86f235132dbf0888
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-11 20:29:39 +0530

    refractor: Initiated with Doctor Dashboard

--- frontend/src/pages/doctor/DoctorDashboard.jsx
+++ frontend/src/pages/doctor/DoctorDashboard.jsx
@@ -1,6 +1,24 @@
+import { PersonBadge, CurrencyRupee, CalendarCheck, People } from "react-bootstrap-icons"
+import DashboardCard from "../../components/DashboardCard"
 function DoctorDashboard() {
     return (
-        <div>Doctor Dashboard</div>
+        <div className="container-fluid">
+            <div className="row g-4">
+                <div className="col-12 col-sm-6 col-xl-3">
+                    <DashboardCard title={"Total Patients"} count={120} icon={<People />} />
+                </div>
+                <div className="col-12 col-sm-6 col-xl-3">
+                    <DashboardCard title={"Today's Visits"} count={18} icon={<CalendarCheck />} />
+                </div>
+                <div className="col-12 col-sm-6 col-xl-3">
+                    <DashboardCard title={"Pending Consultations"} count={12} icon={<PersonBadge />} />
+                </div>
+                <div className="col-12 col-sm-6 col-xl-3">
+                    <DashboardCard title={"Total Earnings"} count={5000} icon={<CurrencyRupee
+                    />} />
+                </div>
+            </div>
+        </div>
     )
 }
 
