0306f40265c2bb9c987c1b26fdc5a000245c66bf
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-11 20:27:58 +0530

    feat: all services for referral center

---
+++ frontend/src/services/referralCenterService.js
@@ -0,0 +1,43 @@
+import axiosInstance from "../api/axiosConfig";
+
+// Add ReferralCenter
+export const addReferralCenter = async (referralCenterData) => {
+  const response = await axiosInstance.post(
+    "/referral-centers",
+    referralCenterData,
+  );
+  return response.data;
+};
+
+// Get all ReferralCenters
+export const getReferralCenters = async () => {
+  const response = await axiosInstance.get("/referral-centers");
+  return response.data;
+};
+
+//Get ReferralCenter by Id
+export const getReferralCenter = async (id) => {
+  const response = await axiosInstance.get(`/referral-centers/${id}`);
+  return response.data;
+};
+
+//Get ReferralCenter by Doctor Id
+// export const getByDoctorId = async (id) => {
+//   const response = await axiosInstance.get(`/doctors/${id}`);
+//   return response.data;
+// };
+
+// Update ReferralCenter by Id
+export const updateReferralCenter = async (id, referralCenterData) => {
+  const response = await axiosInstance.put(
+    `/referral-centers/${id}`,
+    referralCenterData,
+  );
+  return response.data;
+};
+
+//Delete ReferralCenter by Id
+export const deleteReferralCenter = async (id) => {
+  const response = await axiosInstance.delete(`/referral-centers/${id}`);
+  return response.data;
+};
