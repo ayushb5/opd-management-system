80de4e0629801c6be3c86428ae0a207b9008aa5c
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-12 20:53:46 +0530

    Dashboard Cards icons and structure refractor

--- frontend/src/pages/doctor/DoctorDashboard.jsx
+++ frontend/src/pages/doctor/DoctorDashboard.jsx
@@ -1,20 +1,20 @@
-import { PersonBadge, CurrencyRupee, CalendarCheck, People } from "react-bootstrap-icons"
+import { PersonBadge, CurrencyRupee, CalendarCheck, People, Calendar, HourglassSplit } from "react-bootstrap-icons"
 import DashboardCard from "../../components/DashboardCard"
 function DoctorDashboard() {
     return (
         <div className="container-fluid">
             <div className="row g-4">
                 <div className="col-12 col-sm-6 col-xl-3">
-                    <DashboardCard title={"Total Patients"} count={120} icon={<People />} />
+                    <DashboardCard title={"Today's Visits"} count={18} icon={<Calendar />} />
                 </div>
                 <div className="col-12 col-sm-6 col-xl-3">
-                    <DashboardCard title={"Today's Visits"} count={18} icon={<CalendarCheck />} />
+                    <DashboardCard title={"Today's total Patients"} count={120} icon={<People />} />
                 </div>
                 <div className="col-12 col-sm-6 col-xl-3">
-                    <DashboardCard title={"Pending Consultations"} count={12} icon={<PersonBadge />} />
+                    <DashboardCard title={"Pending Consultations"} count={12} icon={<HourglassSplit />} />
                 </div>
                 <div className="col-12 col-sm-6 col-xl-3">
-                    <DashboardCard title={"Total Earnings"} count={5000} icon={<CurrencyRupee
+                    <DashboardCard title={"Completed Visits"} count={6} icon={<CalendarCheck
                     />} />
                 </div>
             </div>
