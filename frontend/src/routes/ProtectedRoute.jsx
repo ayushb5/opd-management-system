14f7d6c39b91858cc710a60b282dfdb617d6c4a1
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-14 21:50:21 +0530

    refractor: modified with localstorage

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,15 +1,22 @@
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children, allowedRole }) {
+
     const token = localStorage.getItem("token");
-    const role = localStorage.getItem("role");
+
+    const user = JSON.parse(
+        localStorage.getItem("user")
+    );
+
     if (!token) {
-        return <Navigate to="/" replace />
-    }
-    if (role !== allowedRole) {
         return <Navigate to="/" replace />;
     }
+
+    if (!user || user.role !== allowedRole) {
+        return <Navigate to="/" replace />;
+    }
+
     return children;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
