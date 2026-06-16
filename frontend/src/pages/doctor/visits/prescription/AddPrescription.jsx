5f25348015154d522b8de5e4c650c9835c5de394
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-16 12:24:52 +0530

    refractor: added functionality for Adding prescription

--- frontend/src/pages/doctor/visits/prescription/AddPrescription.jsx
+++ frontend/src/pages/doctor/visits/prescription/AddPrescription.jsx
@@ -1,38 +1,55 @@
 import { ArrowLeft } from "react-bootstrap-icons";
-import { useNavigate } from "react-router-dom"
-import DoctorForm from "../../../../components/doctor/DoctorForm";
+import { useNavigate, useParams } from "react-router-dom"
 import { toast } from "react-toastify";
-import { addDoctor } from "../../../../services/doctorService";
+import { addPrescription } from "../../../../services/prescriptionService";
+import PrescriptionForm from "../../../../components/prescription/PrescriptionForm";
 
 function AddPrescription() {
-    const user = JSON.parse(localStorage.getItem("user"));
+    const { visitId } = useParams();
 
     const navigate = useNavigate();
     const initialValues = {
-        name: "",
-        email: "",
-        password: "",
-        specialization: "",
-        clinicName: "",
-        address: "",
-        mobileNo: "",
+        visitId,
+
+        medicineId: "",
+
+        doseQuantity: "1",
+        doseUnit: "Tablet",
+
+        morningDose: 1,
+        afternoonDose: 0,
+        eveningDose: 1,
+
+        durationDays: 5,
+
+        instructions: "",
+        quantityNote: ""
     }
 
-    const handleSubmit = async (values, { setSubmitting }) => {
+    const handleSubmit = async (
+        values,
+        { setSubmitting }
+    ) => {
         try {
-            await addDoctor(values);
-            toast.success("Doctor added successfully");
-            navigate("/admin/doctors/")
+            await addPrescription(values);
+
+            toast.success(
+                "Prescription added successfully"
+            );
+
+            navigate(`/doctor/visits/${visitId}`);
         } catch (error) {
             toast.error(
-                error.response?.data?.message || "Failed to add doctor"
+                error.response?.data?.message ||
+                "Failed to add prescription"
             );
-            console.error(error);
-            console.log(error.response.data);
+            console.log(error.response?.data);
+            console.log(error.response?.status);
+            console.log(error.config?.data);
         } finally {
             setSubmitting(false);
         }
-    }
+    };
     return (
         <>
             <div className="d-flex justify-content-between align-items-center">
@@ -40,13 +57,13 @@ function AddPrescription() {
                 <button
                     type="button"
                     className="btn btn-outline-secondary"
-                    onClick={() => navigate(`/doctor/visits/${user.id}`)}
+                    onClick={() => navigate(`/doctor/visits/${visitId}`)}
                 >
                     <ArrowLeft className="me-2" />
                     Go Back
                 </button>
             </div>
-            <DoctorForm initialValues={initialValues} onSubmit={handleSubmit} />
+            <PrescriptionForm initialValues={initialValues} onSubmit={handleSubmit} />
         </>
     )
 }
