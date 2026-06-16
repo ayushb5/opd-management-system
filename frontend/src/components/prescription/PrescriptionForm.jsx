57c80bc572ff53fb5e277cbb2016e186b834e270
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-16 12:27:26 +0530

    Added folder prescription for reusable component

---
+++ frontend/src/components/prescription/PrescriptionForm.jsx
@@ -0,0 +1,224 @@
+import { useFormik } from "formik"
+import { prescriptionValidationSchema } from "../../validations/prescriptionValidation"
+import { useEffect, useState } from "react";
+import { getMedicines } from "../../services/medicineService"
+import { NavLink } from "react-router-dom";
+
+function PrescriptionForm({ initialValues, onSubmit, isEdit = false }) {
+    const formik = useFormik({
+        initialValues,
+        validationSchema: prescriptionValidationSchema,
+        onSubmit,
+        enableReinitialize: true
+    })
+
+    const [medicines, setMedicines] = useState([]);
+
+    useEffect(() => {
+        fetchMedicines();
+    }, []);
+
+    const fetchMedicines = async () => {
+        try {
+            const medicineData = await getMedicines();
+            setMedicines(medicineData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const totalQuantity =
+        (
+            Number(formik.values.morningDose || 0) +
+            Number(formik.values.afternoonDose || 0) +
+            Number(formik.values.eveningDose || 0)
+        ) *
+        Number(formik.values.durationDays || 0);
+
+    return (
+        <form onSubmit={formik.handleSubmit} className="mt-4">
+            <div className="row g-3">
+                <div className="col-lg-6">
+                    <label htmlFor="medicineId" className="form-label">
+                        Medicine <span className="text-danger">*</span>
+                    </label>
+                    <select
+                        name="medicineId"
+                        value={formik.values.medicineId}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className="form-select border-black"
+                    >
+                        <option value="">Select Medicine</option>
+
+                        {medicines.map((medicine) => (
+                            <option key={medicine.id} value={medicine.id}>
+                                {medicine.medicineName}
+                            </option>
+                        ))}
+                    </select>
+
+                    {formik.touched.medicineId && formik.errors.medicineId && (
+                        <div className="text-danger">{formik.errors.medicineId}</div>
+                    )}
+                </div>
+                <div className="col-lg-6">
+                    <label htmlFor="doseQuantity" className="form-label">
+                        Dose Quantity <span className="text-danger">*</span>
+                    </label>
+
+                    <input
+                        type="number"
+                        name="doseQuantity"
+                        className="form-control border-black"
+                        value={formik.values.doseQuantity}
+                        onChange={formik.handleChange}
+                        min={1}
+                    />
+                </div>
+            </div>
+
+            <div className="row g-3 mt-1">
+                <div className="col-lg-6">
+                    <label htmlFor="doseUnit" className="form-label">
+                        Dose Unit <span className="text-danger">*</span>
+                    </label>
+
+                    <select
+                        name="doseUnit"
+                        className="form-select border-black"
+                        value={formik.values.doseUnit}
+                        onChange={formik.handleChange}
+                    >
+                        <option value="Tablet">Tablet</option>
+                        <option value="Capsule">Capsule</option>
+                        <option value="Syrup">Syrup</option>
+                        <option value="Injection">Injection</option>
+                    </select>
+                </div>
+                <div className="col-lg-6">
+                    <label htmlFor="durationDays" className="form-label">
+                        Duration Days <span className="text-danger">*</span>
+                    </label>
+
+                    <input
+                        type="number"
+                        name="durationDays"
+                        className="form-control border-black"
+                        value={formik.values.durationDays}
+                        onChange={formik.handleChange}
+                        min={1}
+                    />
+                </div>
+            </div>
+            <div className="row g-3 mt-1">
+                <div className="col-lg-4">
+                    <label htmlFor="morningDose" className="form-label">
+                        Morning
+                    </label>
+
+                    <input
+                        type="number"
+                        name="morningDose"
+                        className="form-control border-black"
+                        value={formik.values.morningDose}
+                        onChange={formik.handleChange}
+                        min={0}
+                    />
+                </div>
+                <div className="col-lg-4">
+                    <label htmlFor="afternoonDose" className="form-label">
+                        Afternoon
+                    </label>
+
+                    <input
+                        type="number"
+                        name="afternoonDose"
+                        className="form-control border-black"
+                        value={formik.values.afternoonDose}
+                        onChange={formik.handleChange}
+                        min={0}
+                    />
+                </div>
+                <div className="col-lg-4">
+                    <label htmlFor="eveningDose" className="form-label">
+                        Evening
+                    </label>
+
+                    <input
+                        type="number"
+                        name="eveningDose"
+                        className="form-control border-black"
+                        value={formik.values.eveningDose}
+                        onChange={formik.handleChange}
+                        min={0}
+                    />
+                </div>
+            </div>
+
+            <div className="mt-3">
+                <label htmlFor="instructions" className="form-label">
+                    Instructions
+                </label>
+
+                <textarea
+                    rows="3"
+                    name="instructions"
+                    className="form-control border-black"
+                    value={formik.values.instructions}
+                    onChange={formik.handleChange}
+                />
+            </div>
+
+            <div className="mt-3">
+                <label htmlFor="quantityNote" className="form-label">
+                    Quantity Note
+                </label>
+
+                <textarea
+                    rows="2"
+                    name="quantityNote"
+                    className="form-control border-black"
+                    value={formik.values.quantityNote}
+                    onChange={formik.handleChange}
+                />
+            </div>
+
+            <div className="alert alert-info mt-4">
+
+                <strong>Dosage:</strong>
+
+                {" "}
+                {formik.values.doseQuantity}
+                {" "}
+                {formik.values.doseUnit}
+                (
+                {formik.values.morningDose}
+                -
+                {formik.values.afternoonDose}
+                -
+                {formik.values.eveningDose}
+                )
+
+                <br />
+
+                <strong>Total Quantity:</strong>
+
+                {" "}
+                {totalQuantity}
+            </div>
+
+            <div className="text-center mt-5">
+                <button
+                    type="submit"
+                    className="btn btn-primary px-4"
+                    disabled={formik.isSubmitting}
+                >
+                    {isEdit ? "Update Prescription" : "Add Prescription"}
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default PrescriptionForm
