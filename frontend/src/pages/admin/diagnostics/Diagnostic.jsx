839812ca1e3d17b653eb66945d6eb8a944eb502d
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-18 12:46:38 +0530

    feat: Display Diagnostic module inside admin

---
+++ frontend/src/pages/admin/diagnostics/Diagnostic.jsx
@@ -0,0 +1,9 @@
+import DiagnosticList from "../../../components/diagnostic/DiagnosticList"
+
+function Diagnostic() {
+    return (
+        <DiagnosticList />
+    )
+}
+
+export default Diagnostic
