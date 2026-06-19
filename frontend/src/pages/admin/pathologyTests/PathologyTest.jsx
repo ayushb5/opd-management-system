e6b769e90786befd5d956095c332252ea00e757c
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-19 21:16:06 +0530

    feat: Added pathology test inside Admin

---
+++ frontend/src/pages/admin/pathologyTests/PathologyTest.jsx
@@ -0,0 +1,9 @@
+import PathologyTestList from "../../../components/pathologyTest/PathologyTestList"
+
+function PathologyTest() {
+    return (
+        <PathologyTestList />
+    )
+}
+
+export default PathologyTest
