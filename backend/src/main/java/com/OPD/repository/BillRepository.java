1d5e154cc270f5d17980581f54a4fb5a5732300e
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-26 19:34:13 +0530

    refractor: added important methods for dashboard

--- backend/src/main/java/com/OPD/repository/BillRepository.java
+++ backend/src/main/java/com/OPD/repository/BillRepository.java
@@ -1,10 +1,17 @@
 package com.OPD.repository;
 
+import java.math.BigDecimal;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.OPD.entities.Bill;
 @Repository
 public interface BillRepository extends JpaRepository<Bill, Integer> {
 	Bill findByVisit_Id(Integer visitId);
+	
+	@Query("SELECT COALESCE(SUM(b.paidAmount), 0) FROM Bill b")
+	BigDecimal sumPaidAmount();
+	long countByPaymentStatus(String paymentStatus);
 }
