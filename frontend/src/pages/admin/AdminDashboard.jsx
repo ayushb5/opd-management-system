18f6460a261a9ac3166e7c5e17323549a8297dc7
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-27 20:20:06 +0530

    refractor: added cards, and recent visit table inside admin dashboard

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,23 +1,50 @@
+import { useEffect, useState } from "react"
 import DashboardCard from "../../components/DashboardCard"
-import { PersonBadge, CurrencyRupee, CalendarCheck, People } from "react-bootstrap-icons"
+import { PersonBadge, CalendarCheck, People, HourglassSplit } from "react-bootstrap-icons"
+import { getDashboard } from "../../services/dashboardService";
+import RecentVisitTable from "../../components/RecentVisitTable";
+
 function AdminDashboard() {
+    const [dashboard, setDashboard] = useState({
+        todayVisits: 0,
+        totalPatients: 0,
+        totalDoctors: 0,
+        pendingVisits: 0,
+        recentVisits: []
+    });
+
+    useEffect(() => {
+        fetchDashboardStats();
+    }, [])
+
+    const fetchDashboardStats = async () => {
+        try {
+            const response = await getDashboard();
+            setDashboard(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <div className="container-fluid">
-            <div className="row g-4">
+            <div className="row g-4 mb-4">
                 <div className="col-12 col-sm-6 col-xl-3">
-                    <DashboardCard title={"Total Visits"} count={18} icon={<CalendarCheck />} />
+                    <DashboardCard title={"Today's Visits"} count={dashboard.todayVisits} icon={<CalendarCheck />} />
                 </div>
                 <div className="col-12 col-sm-6 col-xl-3">
-                    <DashboardCard title={"Total Doctors"} count={12} icon={<PersonBadge />} />
+                    <DashboardCard title={"Total Patients"} count={dashboard.totalPatients} icon={<People />} />
                 </div>
                 <div className="col-12 col-sm-6 col-xl-3">
-                    <DashboardCard title={"Total Patients"} count={120} icon={<People />} />
+                    <DashboardCard title={"Total Doctors"} count={dashboard.totalDoctors} icon={<PersonBadge />} />
                 </div>
                 <div className="col-12 col-sm-6 col-xl-3">
-                    <DashboardCard title={"Total Earnings"} count={5000} icon={<CurrencyRupee
+                    <DashboardCard title={"Pending Visits"} count={dashboard.pendingVisits} icon={<HourglassSplit
                     />} />
                 </div>
             </div>
+
+            <RecentVisitTable recentVisits={dashboard.recentVisits} />
         </div>
     )
 }
