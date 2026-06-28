aabb949850524405f6346526e485b96b018146bf
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-28 20:10:36 +0530

    refractor: added services to get Dashboard stats

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -1,5 +1,13 @@
 import axiosInstance from "../api/axiosConfig";
 
-export const getDashboard = () => {
+export const getAdminDashboard = () => {
   return axiosInstance.get("/admin/dashboard");
+};
+
+export const getDoctorDashboard = (doctorId) => {
+  return axiosInstance.get(`/doctors/dashboard/${doctorId}`);
+};
+
+export const getReceptionistDashboard = () => {
+  return axiosInstance.get("/receptionists/dashboard");
 };
