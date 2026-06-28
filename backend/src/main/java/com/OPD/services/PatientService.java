267394f7d949e8675e779888b7fb315bc85d00ee
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-28 19:58:28 +0530

    refractor: GetPatients modification with pagination functionality

--- backend/src/main/java/com/OPD/services/PatientService.java
+++ backend/src/main/java/com/OPD/services/PatientService.java
@@ -1,11 +1,14 @@
 package com.OPD.services;
 
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+
 import com.OPD.entities.Patient;
 
 public interface PatientService {
 	Patient save(Patient patient);
-	List<Patient> getAll();
+	Page<Patient> getAll(int page, int size, String search);
 	Patient getById(Integer id);
 	List<Patient> getPatientByDoctorId(Integer doctorId);
 	void deletePatientById(Integer id);
