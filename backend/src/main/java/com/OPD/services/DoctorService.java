90922deba3b03632f6a4d078cfe9ea5200b460d2
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-29 10:42:40 +0530

    refractor: added functionality to get all doctors with pagination and search

--- backend/src/main/java/com/OPD/services/DoctorService.java
+++ backend/src/main/java/com/OPD/services/DoctorService.java
@@ -2,11 +2,13 @@ package com.OPD.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import com.OPD.entities.Doctor;
 
 public interface DoctorService {
 	Doctor save(Doctor doctor);
-	List<Doctor> getAllDoctors();
+	Page<Doctor> getAllDoctors(int page,int size,String search);
 	Doctor getDoctorById(Integer id);
 	void deleteDoctorById(Integer id);
 }
