86aedec73234dcea3985acb1242240429c573ea2
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-29 10:43:54 +0530

    refractor: added method to get doctor with searching functionality

--- backend/src/main/java/com/OPD/repository/DoctorRepository.java
+++ backend/src/main/java/com/OPD/repository/DoctorRepository.java
@@ -2,6 +2,8 @@ package com.OPD.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,11 @@ import com.OPD.entities.Doctor;
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
 	Optional<Doctor> findByEmail(String email);
+	
+	Page<Doctor> findByNameContainingIgnoreCaseOrSpecializationContainingIgnoreCaseOrMobileNoContaining(
+	        String name,
+	        String specialization,
+	        String mobileNo,
+	        Pageable pageable
+	);
 }	
