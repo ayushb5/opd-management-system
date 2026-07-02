a92f533707f6e7d5663d87df036054b36af39d25
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-07-02 21:36:36 +0530

    feat: added service for admin methods

---
+++ frontend/src/services/adminService.js
@@ -0,0 +1,19 @@
+import axiosInstance from "../api/axiosConfig";
+
+//Get admin by Id
+export const getAdminById = async (id) => {
+  const response = await axiosInstance.get(`/admin/${id}`);
+  return response.data;
+};
+
+// Update admin by Id
+export const updateAdmin = async (id, adminData) => {
+  const response = await axiosInstance.put(`/admin/${id}`, adminData);
+  return response.data;
+};
+
+//Delete admin by Id
+export const deleteAdmin = async (id) => {
+  const response = await axiosInstance.delete(`/admin/${id}`);
+  return response.data;
+};
