80a28bf908abe77d1d89ce010a2fde852a9c590f
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-07-03 20:50:51 +0530

    feat: added dto to change password fields for profile

---
+++ backend/src/main/java/com/OPD/dto/ChangePasswordDto.java
@@ -0,0 +1,52 @@
+package com.OPD.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class ChangePasswordDto {
+	 @NotBlank(message = "Current password is required")
+	    private String currentPassword;
+
+	 @NotBlank(message = "New password is required")
+	 @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+	    private String newPassword;
+
+	 @NotBlank(message = "Confirm password is required")
+	    private String confirmPassword;
+	 
+	 public ChangePasswordDto(){
+	 }
+
+	 public String getCurrentPassword() {
+		 return currentPassword;
+	 }
+
+	 public void setCurrentPassword(String currentPassword) {
+		 this.currentPassword = currentPassword;
+	 }
+
+	 public String getNewPassword() {
+		 return newPassword;
+	 }
+
+	 public void setNewPassword(String newPassword) {
+		 this.newPassword = newPassword;
+	 }
+
+	 public String getConfirmPassword() {
+		 return confirmPassword;
+	 }
+
+	 public void setConfirmPassword(String confirmPassword) {
+		 this.confirmPassword = confirmPassword;
+	 }
+
+	 public ChangePasswordDto(@NotBlank(message = "Current password is required") String currentPassword,
+			@NotBlank(message = "New password is required") @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters") String newPassword,
+			@NotBlank(message = "Confirm password is required") String confirmPassword) {
+		super();
+		this.currentPassword = currentPassword;
+		this.newPassword = newPassword;
+		this.confirmPassword = confirmPassword;
+	 }
+}
