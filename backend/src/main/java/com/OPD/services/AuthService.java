886c54b90436b70d524b185b11501aa24b3724a1
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-07-04 21:02:23 +0530

    feat: added service for to get Login Response for verifyOtp

--- backend/src/main/java/com/OPD/services/AuthService.java
+++ backend/src/main/java/com/OPD/services/AuthService.java
@@ -1,11 +1,14 @@
 package com.OPD.services;
 
 import com.OPD.dto.LoginDto;
+import com.OPD.dto.VerifyOtpDto;
+import com.OPD.response.LoginOtpResponse;
 import com.OPD.response.LoginResponse;
 
 public interface AuthService {
 //	LoginResponse adminLogin(LoginDto loginDto);
 //	LoginResponse doctorLogin(LoginDto loginDto);
 //	LoginResponse receptionistLogin(LoginDto loginDto);
-	LoginResponse login(LoginDto loginDto);
+	LoginOtpResponse login(LoginDto loginDto);
+	LoginResponse verifyOtp(VerifyOtpDto dto);
 }
