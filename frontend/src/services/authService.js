1da83a85338652d48fca10700047925d792a52ca
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-07-04 20:59:31 +0530

    refractor: added services in auth service related to forgot password and verify otp

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -5,3 +5,27 @@ export const login = async (loginData) => {
   const response = await axiosInstance.post("/auth/login", loginData);
   return response.data;
 };
+
+// forgot password
+export const forgotPassword = async (data) => {
+  const response = await axiosInstance.post("/auth/forgot-password", data);
+  return response.data;
+};
+
+export const validateResetToken = async (token) => {
+  const response = await axiosInstance.get("/auth/validate-reset-token", {
+    params: { token },
+  });
+  return response.data;
+};
+
+export const resetPassword = async (data) => {
+  const response = await axiosInstance.post("/auth/reset-password", data);
+  return response.data;
+};
+
+// Verify otp
+export const verifyOtp = async (data) => {
+  const response = await axiosInstance.post("/auth/verify-otp", data);
+  return response.data;
+};
