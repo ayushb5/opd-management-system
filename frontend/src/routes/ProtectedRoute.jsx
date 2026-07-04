892a77da41806d2c735ed007d129d8b527e057a5
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-07-04 20:58:42 +0530

    refractor: added feature getting user from session Storage

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -2,11 +2,11 @@ import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children, allowedRole }) {
 
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token") || sessionStorage.getItem("token");
 
-    const user = JSON.parse(
-        localStorage.getItem("user")
-    );
+    const user =
+        JSON.parse(localStorage.getItem("user")) ||
+        JSON.parse(sessionStorage.getItem("user"));
 
     if (!token) {
         return <Navigate to="/" replace />;
