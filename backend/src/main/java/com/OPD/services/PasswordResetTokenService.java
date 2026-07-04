2f701599b412d257de50a18203042ba1b448d2d4
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-07-04 21:01:48 +0530

    feat: added serrvice for reset password

--- backend/src/main/java/com/OPD/services/PasswordResetTokenService.java
+++ backend/src/main/java/com/OPD/services/PasswordResetTokenService.java
@@ -1,7 +1,9 @@
 package com.OPD.services;
 
+import com.OPD.dto.ResetPasswordDto;
+
 public interface PasswordResetTokenService {
 	void createResetToken(String email);
 	void validateToken(String token);
-//	void resetPassword(ResetPasswordDto dto);
+	void resetPassword(ResetPasswordDto dto);
 }
